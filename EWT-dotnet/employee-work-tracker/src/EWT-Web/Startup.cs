89ca13b5273eb3c84bf88e1bbdda25f1a2463853
Jakub Stawiski <jkstav9@gmail.com>
2021-07-15 12:13:39 +0200

    feat: add MediatR

--- EWT-dotnet/employee-work-tracker/src/EWT-Web/Startup.cs
+++ EWT-dotnet/employee-work-tracker/src/EWT-Web/Startup.cs
@@ -1,3 +1,4 @@
+using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -26,7 +27,7 @@ namespace EWT_Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddMediatR(typeof(Startup));
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
