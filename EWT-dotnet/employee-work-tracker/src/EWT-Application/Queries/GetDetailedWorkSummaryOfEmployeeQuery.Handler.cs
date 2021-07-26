6fcc55b75dba6984cbdf5bdeb448c2301ee06acd
Jakub Stawiski <jkstav9@gmail.com>
2021-07-26 17:27:18 +0200

    feat: add detailed work summary of employee

---
+++ EWT-dotnet/employee-work-tracker/src/EWT-Application/Queries/GetDetailedWorkSummaryOfEmployeeQuery.Handler.cs
@@ -0,0 +1,61 @@
+﻿using EWT_Application.Out;
+using EWT_Domain;
+using FluentResults;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EWT_Application.Queries
+{
+    public class GetDetailedWorkSummaryOfEmployeeQueryHandler : IRequestHandler<GetDetailedWorkSummaryOfEmployeeQuery, Result<DetailedWorkSummary>>
+    {
+        private readonly IPersistWorkEventPort persistWorkEventPort;
+
+        //todo do odzielnego pliku
+        private static readonly EventToWorkDurationDecision workDayEndsAt_3am =
+            (eventToJudge, date) => eventToJudge.DateTimeInstant < date.AddDays(1).AddHours(3);
+
+        public GetDetailedWorkSummaryOfEmployeeQueryHandler(IPersistWorkEventPort persistWorkEventPort)
+        {
+            this.persistWorkEventPort = persistWorkEventPort;
+        }
+
+        public async Task<Result<DetailedWorkSummary>> Handle(GetDetailedWorkSummaryOfEmployeeQuery request, CancellationToken cancellationToken)
+        {
+            DateTime fromInclusiveUtc = request.FromInclusive.ToUniversalTime();
+            DateTime toInclusiveUtc = request.ToInclusive.ToUniversalTime();
+
+            var result = await persistWorkEventPort.GetWorkEventsOfEmployeeBetweenAsync(
+                request.EmployeeId, fromInclusiveUtc, toInclusiveUtc);
+
+            if (result.IsFailed)
+                return result.ToResult<DetailedWorkSummary>();
+
+            var workEvents = result.Value;
+
+            WorkTimeAnalyzer workTimeAnalyzer = new WorkTimeAnalyzer(workDayEndsAt_3am);
+            foreach (var workEvent in workEvents)
+                workTimeAnalyzer.AddWorkEvent(workEvent);
+
+            var workDurations = workTimeAnalyzer.GetWorkDurations();
+
+            DetailedWorkSummary detailedWorkSummary = new DetailedWorkSummary(
+                fromInclusiveUtc, toInclusiveUtc, workDurations);
+
+            TimeSpan workDurationSum = TimeSpan.Zero;
+            foreach (var workDuration in workDurations)
+            {
+                TimeSpan? timeSpan = workDuration.AsTimeSpan();
+                if (timeSpan.HasValue)
+                    workDurationSum = workDurationSum.Add(timeSpan.Value);
+            }
+            detailedWorkSummary.MinutesOfWork = (uint)workDurationSum.TotalMinutes;
+
+            return Result.Ok(detailedWorkSummary);
+        }
+    }
+}
---
+++ EWT-dotnet/employee-work-tracker/src/EWT-Application/Queries/GetDetailedWorkSummaryOfEmployeeQuery.cs
@@ -0,0 +1,24 @@
+﻿using FluentResults;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EWT_Application.Queries
+{
+    public class GetDetailedWorkSummaryOfEmployeeQuery : IRequest<Result<DetailedWorkSummary>>
+    {
+        public GetDetailedWorkSummaryOfEmployeeQuery(Guid employeeId, DateTime fromInclusive, DateTime toInclusive)
+        {
+            EmployeeId = employeeId;
+            FromInclusive = fromInclusive;
+            ToInclusive = toInclusive;
+        }
+
+        public Guid EmployeeId { get; }
+        public DateTime FromInclusive { get; }
+        public DateTime ToInclusive { get; }
+    }
+}
